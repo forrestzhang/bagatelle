e92282a51bad7119dd5e84d20c271047a735771a
Tao Zhang <forrestzhang1982@gmail.com>
2016-02-17 12:04:44 -0600

    end region bug fix

--- bagatelle/regioncount.py
+++ bagatelle/regioncount.py
@@ -59,7 +59,7 @@ def midcount(bwfile, bedfile, up=1000, down=1000, winsize=50,
 
         ctregionstart = midsite - up
 
-        ctregionend = midsite + end
+        ctregionend = midsite + down
 
         if ctregionstart < 0 :
 
@@ -70,7 +70,7 @@ def midcount(bwfile, bedfile, up=1000, down=1000, winsize=50,
         if ctregionend > chrlen[chromosome]:
 
             print("%d is too close to %s end %d" % (end, chromosome, chrlen[chromosome]))
-            print(chromosome, start, end, midsite, ctregionstart, ctregionend )
+            # print(chromosome, start, end, midsite, ctregionstart, ctregionend )
             continue
 
         socorelist1 = bw.stats(chromosome, ctregionstart, ctregionend, nBins=nbins)
