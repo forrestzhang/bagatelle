64c1e68cc40f5331d66fd170f822ff6df19e042b
Tao Zhang <forrestzhang1982@gmail.com>
2016-02-25 16:38:37 -0600

    add cutting count with strand

--- bagatelle/bamTobigwig.py
+++ bagatelle/bamTobigwig.py
@@ -8,7 +8,20 @@ Note: pyBigWig only allow float as values
 
 def dhstobw(bamfile, bwfile, library='Duke'):
 
-    # unfinished
+    """
+
+    :param bamfile:
+    :param bwfile:
+    :param library:Duke or Washington
+
+        Duke: |=====>
+                        <=====|
+
+        Washington: |===========|
+
+        Out put cutting site '|'
+    :return:
+    """
 
     bamfor = Baminfo.Baminfo(bamfile)
 
@@ -39,6 +52,78 @@ def dhstobw(bamfile, bwfile, library='Duke'):
                           span=1, step=1)
 
     bw.close()
+
+
+def dhsstrandtobw(bamfile,rbwfile, fbwfile, library='Duke', nagtiverev=True):
+
+    """
+
+    :param bamfile:
+    :param bwfile:
+    :param library:Duke or Washington
+
+        Duke: |=====>
+                        <=====|
+
+        Washington: |===========|
+
+        Out put cutting site '|'
+    :param nagtiverev: - Strand use nagative score
+    :return:
+    """
+
+    bamfor = Baminfo.Baminfo(bamfile)
+
+    bwf = pyBigWig.open(fbwfile, "w")
+
+    bwr = pyBigWig.open(rbwfile, "w")
+
+    bwf.addHeader(list(bamfor.chrlen.items()))
+
+    bwr.addHeader(list(bamfor.chrlen.items()))
+
+    for chromosome in bamfor.chrlen:
+
+        end = bamfor.chrlen[chromosome]
+
+        dhscut = dhsbam.dhcutcount(bamfile=bamfile, chromosome=chromosome, start=1,
+                                         end=end, library=library)
+
+        if dhscut['+']:
+
+            starts = list()
+
+            values = list()
+
+            for start in sorted(dhscut):
+
+                starts.append(start)
+
+                values.append(float(dhscut[start]))
+
+            bwf.addEntries(chromosome, starts=starts, values=values,
+                          span=1, step=1)
+
+        if dhscut['-']:
+
+            starts = list()
+
+            values = list()
+
+            for start in sorted(dhscut):
+
+                starts.append(start)
+                if nagtiverev:
+                    values.append(0-float(dhscut[start]))
+                else:
+                    values.append(float(dhscut[start]))
+
+            bwr.addEntries(chromosome, starts=starts, values=values,
+                          span=1, step=1)
+
+    bwf.close()
+
+    bwr.close()
 
 
 def midtobw(bamfile, bwfile, maxinsert, mininsert, paired=False):
--- bagatelle/dhsbam.py
+++ bagatelle/dhsbam.py
@@ -55,3 +55,77 @@ def dhcutcount(bamfile, chromosome, start, end, library='Duke'):
         pass
 
     return readscount
+
+
+
+def dhstrandcutcount(bamfile, chromosome, start, end, library='Duke'):
+    """
+
+    :param bamfile: bamfile
+    :param chromosome:
+    :param start:
+    :param end:
+    :param library: Duke or Washington
+
+        Duke: |=====>
+                        <=====|
+
+        Washington: |===========|
+
+        Out put cutting site '|'
+
+    :return: dictionary of cutting site count
+    """
+
+    samfile = openBam.openBam(bamfile)
+
+    readscount = dict()
+
+    readscount['+'] = dict()
+
+    readscount['-'] = dict()
+
+    if library == 'Duke':
+
+        for aligned_read in samfile.fetch(reference=str(chromosome), start=start, end=end):
+
+            if aligned_read.is_reverse:
+
+                site = aligned_read.aend
+
+                site = site + 1
+
+                if site in readscount['-']:
+
+                    readscount['-'][site] = readscount['-'][site] + 1
+
+                else:
+
+                    readscount['-'][site] = 1
+
+            else:
+
+                site = aligned_read.pos
+
+                site = site + 1
+
+                if site in readscount['+']:
+
+                    readscount['+'][site] = readscount['+'][site] + 1
+
+                else:
+
+                    readscount['+'][site] = 1
+
+
+
+    elif library == 'Washington':
+
+        pass
+
+    else:
+
+        pass
+
+
+    return readscount
