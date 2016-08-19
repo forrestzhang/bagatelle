b674a34d4abc4263ca99f82e5d1bfad04e91de18
Tao Zhang <forrestzhang1982@gmail.com>
2016-08-19 17:52:10 -0500

    add install .

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(name='bagatelle',
       version='0.0.1',
       packages=find_packages(),
       py_modules=['bagatelle'],
+      author='Tao Zhang',
+      author_email='forrestzhang1982@gmail.com',
+      url='https://github.com/forrestzhang/bagatelle'
       )
