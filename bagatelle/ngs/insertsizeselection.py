84842f00ea65a5b1412686bbacb5639403ca73e3
Tao Zhang <forrest_zhang@163.com>
2018-05-13 18:03:38 +0800

    add size selection

---
+++ bagatelle/ngs/insertsizeselection.py
@@ -0,0 +1,96 @@
+import sys
+import pysam
+from os import path
+import argparse
+
+
+def main():
+
+    args = check_options(get_options())
+
+    inbamfle = args.inbamfile
+
+    outbamfile = args.outbamfile
+
+    maxinsert = args.maxinsert
+
+    mininsert = args.mininsert
+
+    print("start size selection, input=%s output=%s maxinsert=%s mininsert=%s"
+          % (inbamfle, outbamfile, maxinsert, mininsert))
+
+    insersize(inbamfle, outbamfile, maxinsert, mininsert)
+
+    print("start size selection, input=%s output=%s maxinsert=%s mininsert=%s finished!"
+          % (inbamfle, outbamfile, maxinsert, mininsert))
+
+
+
+def check_options(parser):
+
+    args = parser.parse_args()
+
+    if not path.exists(args.inbamfile):
+
+        print("Can not located bamfile, please input full path of bamfile")
+
+        parser.print_help()
+
+        sys.exit(1)
+
+def get_options():
+
+    parser = argparse.ArgumentParser(description="bagatelle package for insert size selection", prog='bagatelle')
+
+    parser.add_argument('-i', dest='inbamfile', required=True, type=str, help='input bamfile')
+
+    parser.add_argument('-o', dest='outbamfile', required=True, type=str, help='output bamfile')
+
+    parser.add_argument('-l', dest='mininsert', required=True, type=int, default=1, help='min insert size')
+
+    parser.add_argument('-m', dest='maxinsert', required=True, type=int, default=200, help='max insert size')
+
+    return parser
+
+def insersize(inbamfile, outbamfile, maxinsert, mininsert, paired=True):
+    """
+
+    :param bamfile:
+    :param chromosome:
+    :param start:
+    :param end:
+    :param paired:
+    :return: dictionary of mhs middle site count
+    """
+
+    samfile = pysam.AlignmentFile(inbamfile)
+
+    outbam = pysam.AlignmentFile(outbamfile, "wb", template=samfile)
+
+    if paired:
+
+        for aligened_read in samfile.fetch():
+
+            if aligened_read.is_proper_pair:
+
+                insize = abs(aligened_read.isize)
+
+                if mininsert <= insize <= maxinsert:
+
+                        outbam.write(aligened_read)
+
+    samfile.close()
+    outbam.close()
+
+
+if __name__ == '__main__':
+
+    try:
+
+        main()
+
+    except KeyboardInterrupt:
+
+        sys.stderr.write("User interrupt\n")
+
+        sys.exit(0)
---
+++ bagatelle/tgs/blastnremovevectorround2.py
@@ -0,0 +1,132 @@
+from pyfasta import Fasta
+import argparse
+from os import path
+import sys
+
+
+# blastn -db vector.fa -query removehost_removevector_minscore45.1--1.fa -num_threads 40 -task blastn  -outfmt 6 | awk '{print $1"\t"$7"\t"$8}'  | bedtools sort -i - |  bedtools merge -i - -d 1000 > removed_minscore45.1--1.bed
+
+def removehost(fasta, bed, minilength):
+    removeregion = dict()
+
+    with open(bed) as bedin:
+        for i in bedin:
+            (seqname, seqstart, seqend) = i.rstrip().split('\t')[0:3]
+
+            if seqname in removeregion:
+
+                removeregion[seqname].append(i.rstrip())
+            else:
+                removeregion[seqname] = list()
+                removeregion[seqname].append(i.rstrip())
+
+    fa = Fasta(fasta)
+
+    outfile = 'removevector_' + fasta
+
+    outio = open(outfile, 'w')
+
+    for seqname in fa.keys():
+
+        if seqname in removeregion:
+
+            if len(removeregion[seqname]) > 1:
+
+                continue
+
+            else:
+
+                (seqname, seqstart, seqend) = removeregion[seqname][0].split('\t')[0:3]
+
+                seqlen = len(removeregion[seqname])
+
+                mod = list()
+
+                seqstart = int(seqstart)
+
+                seqend = int(seqend)
+
+                # sl = skip left, sr = skip right
+
+                if seqend - seqstart < minilength:
+
+                    continue
+
+                else:
+
+                    lseq = fa[seqname][:seqstart]
+                    rseq = fa[seqname][seqend:]
+                    outst = ''
+                    if len(lseq) > minilength:
+                        lname = seqname + '_L'
+                        outst = outst + '>' + lname + '\n' + lseq + '\n'
+
+                    if len(rseq) > minilength:
+                        rname = seqname + '_R'
+                        outst = outst + '>' + rname + '\n' + rseq + '\n'
+
+
+
+        else:
+
+            outst = '>' + seqname + '\n' + str(fa[seqname]) + '\n'
+
+            outio.write(outst)
+
+    outio.close()
+
+
+def main():
+    args = check_options(get_options())
+
+    removehost(args.fasta, args.bed, args.minilength)
+
+
+def get_options():
+    parser = argparse.ArgumentParser(description="Remove vector in PacBio or Nanopore", prog='RemoveVector')
+
+    parser.add_argument('-f', '--fasta', dest='fasta', help='input fasta',
+                        required=True, type=str)
+    parser.add_argument('-b', '--bed', dest='bed', help='bedfile',
+                        required=True, type=str)
+    parser.add_argument('-l', '--length', dest='minilength', help='bedfile', default=1000,
+                        required=True, type=int)
+
+    return (parser)
+
+
+def check_options(parser):
+    args = parser.parse_args()
+
+    if args.fasta:
+
+        if not path.exists(args.fasta):
+            print("Can not locate input file, please input input file.\n")
+
+            parser.print_help()
+
+            sys.exit(1)
+
+    if args.bed:
+
+        if not path.exists(args.bed):
+            print("Can not locate input file, please input bed file.\n")
+
+            parser.print_help()
+
+            sys.exit(1)
+
+    return args
+
+
+if __name__ == "__main__":
+
+    try:
+
+        main()
+
+    except KeyboardInterrupt:
+
+        sys.stderr.write("User interrupt\n")
+
+        sys.exit(0)
